5e678b2424446654830356f4ef0c977346de94ab
practicalcodewishshiva <practicalcodewithshiva@gmail.com>
2026-04-21 09:07:12 +0530

    fixed modal popup issues

--- hdfc_bank/src/App.jsx
+++ hdfc_bank/src/App.jsx
@@ -1,38 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
-import Button from "@mui/material/Button";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
 import CustomTabs from "./Components/CustomTabs/CustomTabs";
 import CustomCard from "./Components/CustomCard/CustomCard";
 import ExploreAllHdfcBank from "./Components/ExploreAllHdfcBank/ExploreAllHdfcBank";
 import SimplyFinanance from "./Components/SimplyFinanance/SimplyFinanance";
+import Login from "./Components/Login/Login";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <NavBar />
-      <CustomTabs />
 
-      <ExploreAllHdfcBank />
-      <SimplyFinanance />
+      <Routes>
+        <Route path="/" element={<CustomTabs />} />
+        <Route path="/cards" element={<CustomCard />} />
+        <Route path="/explore" element={<ExploreAllHdfcBank />} />
+        <Route path="/finance" element={<SimplyFinanance />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
 
 export default App;
-
-// Components ---
-
-// Navbar
-// tabs
-
-// cards
-
-// installation -- material ui npm i material ui
-
-ExploreAllHdfcBank;
---
+++ hdfc_bank/src/Components/CustomModalPopup/CustomModalPopup.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Popover from "@mui/material/Popover";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import { useNavigate } from "react-router-dom";
+
+const emails = ["Net Banking", "Netbanking For Business", "Credit Card portal"];
+
+export default function CustomModalPopup() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const navigate = useNavigate();
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleNavigate = () => {
+    navigate("/login");
+    handleClose();
+  };
+
+  const open = Boolean(anchorEl);
+
+  return (
+    <div>
+      <Button
+        variant="outlined"
+        onClick={handleClick}
+        style={{ background: "white", marginLeft: "9px", marginTop: "10px" }}
+      >
+        Open Login Options
+      </Button>
+
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <List>
+          {emails.map((email) => (
+            <ListItem key={email} disablePadding>
+              <ListItemButton onClick={handleNavigate}>
+                <ListItemText primary={email} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Popover>
+    </div>
+  );
+}
