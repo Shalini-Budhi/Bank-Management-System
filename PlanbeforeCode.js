abbc7d568a14a2ec1c9346cd22e70a5d3c1c2ebb
madhavareddyrb <madhavareddyrb@gmail.com>
2026-04-24 20:57:05 +0530

    Worked On Plan Of Action

--- PlanbeforeCode.js
+++ PlanbeforeCode.js
@@ -68,3 +68,25 @@
 // CustomerID:String | Number,
 // Password: Number | String
 // }
+
+
+
+// retail-app/ dashboard
+
+// tabs 
+
+// Home, Accounts, Send Money, Cards(dropdown), FD/RD(dropdown), Bills & Recharge, Loans , Invest(dropdown), Insure
+
+// For Every tab create a component and call that in tab items
+
+// Home Tab Steps:
+
+// 1. Welcome UserName last Loggedin data ,  special offer for You (main div , another div(h2,(username, loggedin data), special offer (take flex, jsustify content space arround )))
+
+// 2. Take Main Div Again (tabs, my favourite links). here take grid (for tabs col-8, col-4(links))
+
+// 3. Inside tabs (Accounts, Cards, FD/RD, Loans, Invest, Insure)
+
+// 4. Accounts(3 cards) (show balance(card),split card, Limit cards )
+
+// 5. My Favourite Links ALl are Links with hr(lines brackedown)
