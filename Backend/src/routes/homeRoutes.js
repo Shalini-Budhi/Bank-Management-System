5f7224e35cabe1f8c0f157c55a49c29b1de26754
HarishGowli <gowliharish321@gmail.com>
2026-04-28 20:49:34 +0530

    Implemented /api/home and /api/accounts with mock data

---
+++ Backend/src/routes/homeRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+// Import mock data
+const { user, accounts } = require("../data/mockData");
+
+// HOME API
+router.get("/", (req, res) => {
+  const totalBalance = accounts.reduce((sum, acc) => sum + acc.balance, 0);
+
+  res.status(200).json({
+    user: {
+      name: user.name,
+      lastLogin: user.lastLogin
+    },
+    accountsSummary: {
+      totalBalance,
+      totalAccounts: accounts.length
+    },
+    quickLinks: ["Send Money", "Pay Bills", "Cards"]
+  });
+});
+
+module.exports = router;
