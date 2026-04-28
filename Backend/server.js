dc5148a8520b741b9743371199597116f267f2d1
madhavareddyrb <madhavareddyrb@gmail.com>
2026-04-28 15:59:38 +0530

    Connected Backend DataBase using Drivers non SRV link for everyone

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,15 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
-const bcrypt = require("bcrypt");
-const { connectDB } = require("./src/config/config");
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { connect } = require("http2");
+const connectDB = require("./src/config/config");
+
 const app = require("./src/app");
 
-connectDB();
+console.log("ConnectDB", connectDB());
 
-app.listen(3000, () => {
-  console.log("Server stared check now");
+app.listen(process.env.PORT, () => {
+  console.log("Server started check now");
 });
