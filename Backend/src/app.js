f8ffdce42b99b975b7a2381be080fe0162609f5d
practicalcodewishshiva <practicalcodewithshiva@gmail.com>
2026-04-29 09:51:49 +0530

    backend and frontend integration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,14 +1,23 @@
 const http = require("http");
 const express = require("express");
 const cors = require("cors");
-const { load,login, signup, authMiddleWare,dashboard, holdings, funds } = require("./routes/routes");
+const {
+  load,
+  login,
+  signup,
+  authMiddleWare,
+  dashboard,
+  holdings,
+  funds,
+  cards,
+} = require("./routes/routes");
 const app = express();
 app.use(express.json());
 const homeRoutes = require("./routes/homeRoutes");
 const accountRoutes = require("./routes/accountRoutes");
 
 const corsOptions = {
-  origin: "*", 
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
@@ -21,5 +30,7 @@ app.post("/login", login);
 app.post("/signup", signup);
 app.use("/api/home", homeRoutes);
 app.use("/api/accounts", accountRoutes);
+
+app.get("/cards", cards);
 
 module.exports = app;
--- Backend/src/routes/routes.js
+++ Backend/src/routes/routes.js
@@ -34,7 +34,11 @@ exports.login = async (req, res) => {
     }
 
     if (CustomerID === userObj?.CustomerID) {
-      return res.json({ CustomerID, Password, message: "user fetched succesfully" });
+      return res.json({
+        CustomerID,
+        Password,
+        message: "user fetched succesfully",
+      });
       console.log(userObj.CustomerID, "userObj");
     } else {
       return res.json({ message: "invalid CustomerID address" });
@@ -43,8 +47,6 @@ exports.login = async (req, res) => {
     return res.json({ message: "invalid credentials" });
   }
 };
-
-
 
 exports.signup = async (req, res) => {
   const {
@@ -74,7 +76,16 @@ exports.signup = async (req, res) => {
   }
 };
 
+exports.cards = async (req, res) => {
+  const homeArr = [{ anme: "HomeData", location: "Hyd" }];
+
+  try {
+    res.json({ message: "fetched Home Data succesfully", data: homeArr });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // user singup ---- signupSchema --- data ikkda store chestunam
 
-// loginSchema add chestunam ---login 
+// loginSchema add chestunam ---login
